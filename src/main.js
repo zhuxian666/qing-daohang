fd9ad32ee84323605348d2c146fab411c90c2c82
杨程 <7702269+yangcheng666@user.noreply.gitee.com>
2021-02-04 21:59:31 +0800

    完成手机端

--- src/main.js
+++ src/main.js
@@ -1,72 +1,67 @@
-const $last = $('.last')
-const $siteList = $('.siteList')
-const getStr = JSON.parse(localStorage.getItem('hash'))
+const $last = $(".last");
+const $siteList = $(".siteList");
+const getStr = JSON.parse(localStorage.getItem("hash"));
 const hash = getStr || [
-    {logo: 'B', url: 'https://www.bilibili.com'},
-    {logo: 'W', url: 'https://www.weibo.com'},
-    {logo: 'D', url: 'https://www.douyu.com'},
-    {logo: 'Z', url: 'https://www.zhihu.com/'},
-    {logo: 'X', url: 'https://www.ximalaya.com/'},
-    {logo: 'G', url: 'https://github.com/'},
-
-]
+  { logo: "B", url: "https://www.bilibili.com" },
+  { logo: "W", url: "https://www.weibo.com" },
+  { logo: "D", url: "https://www.douyu.com" },
+  { logo: "Z", url: "https://www.zhihu.com/" },
+  { logo: "X", url: "https://www.ximalaya.com/" },
+  { logo: "G", url: "https://github.com/" },
+];
 const simplifyUrl = (url) => {
-    return url.replace('https://', '')
-        .replace('http://', '')
-        .replace('www.', '')
-        .replace(/\/.*/, '')
-        .replace('.com', '')
-}
+  return url
+    .replace("https://", "")
+    .replace("http://", "")
+    .replace("www.", "")
+    .replace(/\/.*/, "")
+    .replace(".com", "");
+};
 const rander = () => {
-    $siteList.find('.site:not(.last)').remove()
-    hash.forEach((node, index) => {
-        let iconSrc = `${node.url + '/favicon.ico'}`
-        const $li = $(`<li class="site">
+  $siteList.find(".site:not(.last)").remove();
+  hash.forEach((node, index) => {
+    let iconSrc = `${node.url + "/favicon.ico"}`;
+    const $li = $(`<li class="site">
             <div class="icon-wrapper">${node.logo}</div>
             <div class="link">${simplifyUrl(node.url)}</div>
             <svg class="icon close" aria-hidden="true">
                     <use xlink:href="#icon-shanchu"></use>
-                </svg></li>`).insertBefore($last)
-        $li.find('.icon-wrapper').addClass(`${simplifyUrl(node.url)}`)
-        const $img = $(`<img class="icon-wrapper" src="${iconSrc}"/>`)
-        $img.on('load', () => {
-            const $removeClass = $('.site').find(`${'.' + simplifyUrl(iconSrc)}`)
-            $removeClass.replaceWith($img)
-        })
-        $li.on('click', () => {
-            window.open(node.url)
-        })
-        $li.on('click', '.close', (e) => {
-            e.stopPropagation()
-            hash.splice(index, 1)
-            localStorage.setItem('hash', JSON.stringify(hash))
-            rander()
-        })
-    })
-}
-window.onload=()=>{
-    rander()
-}
-$('.addButton').on('click', () => {
-    let url = window.prompt('请输入要添加的网址')
-    if (url && url.indexOf('http') !== 0) {
-        url = 'https://' + url
-    }
-    if (url) {
-        hash.push({
-            logo: simplifyUrl(url)[0].toUpperCase(),
-            url: url
-        })
-        localStorage.setItem('hash', JSON.stringify(hash))
-        rander()
-    }
-})
+                </svg></li>`).insertBefore($last);
+    $li.find(".icon-wrapper").addClass(`${simplifyUrl(node.url)}`);
+    const $img = $(`<img class="icon-wrapper" src="${iconSrc}"/>`);
+    $img.on("load", () => {
+      const $removeClass = $(".site").find(`${"." + simplifyUrl(iconSrc)}`);
+      $removeClass.replaceWith($img);
+    });
+    $li.on("click", () => {
+      window.open(node.url);
+    });
+    $li.on("click", ".close", (e) => {
+      e.stopPropagation();
+      hash.splice(index, 1);
+      localStorage.setItem("hash", JSON.stringify(hash));
+      rander();
+    });
+  });
+};
+window.onload = () => {
+  rander();
+};
+$(".addButton").on("click", () => {
+  let url = window.prompt("请输入要添加的网址");
+  if (url && url.indexOf("http") !== 0) {
+    url = "https://" + url;
+  }
+  if (url) {
+    hash.push({
+      logo: simplifyUrl(url)[0].toUpperCase(),
+      url: url,
+    });
+    localStorage.setItem("hash", JSON.stringify(hash));
+    rander();
+  }
+});
 
 window.onbeforeunload = () => {
-    localStorage.setItem('hash', JSON.stringify(hash))
-}
-
-
-
-
-
+  localStorage.setItem("hash", JSON.stringify(hash));
+};
